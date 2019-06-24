336f5776502a087cf550766af2cde658ca550f7d
Guilherme Neres <guilhermeneresbsb@gmail.com>
2019-06-23 23:29:40 -0300

    Initial projet to learn about tdd

---
+++ src/test/java/br/com/guilherme/tdd/tddlearning/TddLearningApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.guilherme.tdd.tddlearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TddLearningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
